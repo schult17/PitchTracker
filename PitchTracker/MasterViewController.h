107a028ff159355a22c2280fb8e78183c0352f50
Tanner Young-Schultz <tannerys@Tanners-MacBook-Pro-2.local>
2016-04-28 13:31:50 -0400

    Initial Commit - xcode project made

---
+++ PitchTracker/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  PitchTracker
+//
+//  Created by Tanner Young-Schultz on 2016-04-28.
+//  Copyright © 2016 UofTBaseball. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
